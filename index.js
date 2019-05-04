cedaa8d12cf68f195ebb0f216cfa62035133c391
Elduwani <elduwani@gmail.com>
2019-05-04 12:18:46 +0100

    loading list items dynamically from JSON file

--- index.js
+++ index.js
@@ -1,35 +1,57 @@
-const checkboxes = document.querySelectorAll('.checkbox-wrapper input[type=checkbox]')
+const listParent = document.getElementById("list-items");
 
-// console.log(checkboxes)
+function getItems() {
+  fetch("listItems.json")
+    .then(res => res.json())
+    .then(items => {
+      items.forEach(item => {
+        let listChild = document.createElement("li");
+        let newItem = `
+            <div class="checkbox-wrapper">
+                <input type="checkbox" name=${item.id} id="checkbox" />
+            </div>
+            <div class="item-title">${item.title}</div>
+        `;
+        listChild.innerHTML = newItem;
+        listParent.appendChild(listChild);
+      });
+    });
+}
+
+getItems();
+
+const checkboxes = document.querySelectorAll(
+  ".checkbox-wrapper input[type=checkbox]"
+);
+
 let lastChecked;
 
 function handleCheck(e) {
-    
-    const parent = this.parentElement.parentElement;
-    parent.classList.toggle('checked')
-    
-    let inBetween = false;
+  const parent = this.parentElement.parentElement;
+  parent.classList.toggle("checked");
 
-    if(e.shiftKey && this.checked && lastChecked != this) {
-        console.log('Last Checked: ', lastChecked.name)
-        checkboxes.forEach(checkbox => {
-            if (checkbox === this || checkbox === lastChecked) {
-                inBetween = !inBetween;
-            }
-            
-            if (inBetween) {
-                checkbox.checked = true;
-            }
-            
-            if (checkbox.checked) {
-                checkbox.parentElement.parentElement.classList.add('checked')
-            } else {
-                checkbox.parentElement.parentElement.classList.remove('checked')
-            }
+  let inBetween = false;
 
-        })
-    }
-    lastChecked = this;
+  if (e.shiftKey && this.checked && lastChecked != this) {
+    console.log("Last Checked: ", lastChecked.name);
+    checkboxes.forEach(checkbox => {
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+      }
+
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+
+      if (checkbox.checked) {
+        checkbox.parentElement.parentElement.classList.add("checked");
+      } else {
+        checkbox.parentElement.parentElement.classList.remove("checked");
+      }
+    });
+  }
+
+  lastChecked = this;
 }
 
-checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
+checkboxes.forEach(checkbox => checkbox.addEventListener("click", handleCheck));
